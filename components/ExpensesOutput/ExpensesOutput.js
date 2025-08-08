0907d90802f9a0e03739accab1be3b7a7b8160bf
kalaivani35 <kalaivaniaec2024@gmail.com>
2025-08-08 17:20:47 +0530

    first commit

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+
+function ExpensesOutput({expenses}){
+    return <View>
+        <View>
+            <Text>LAST 7 DAYS</Text>
+            <Text>$177.95</Text>
+
+        </View>
+        list of expenses
+    </View>
+
+}
+export default ExpensesOutput;
